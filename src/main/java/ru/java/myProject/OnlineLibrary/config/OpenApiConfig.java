38369e5ea60d787d1aa520071e3f8d37670d5227
Konstantin Pershin <kstprshn@gmail.com>
2024-11-20 12:07:23 +0300

    update OpenApiConfig.java

---
+++ src/main/java/ru/java/myProject/OnlineLibrary/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package ru.java.myProject.OnlineLibrary.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Library API",
+                version = "1.0",
+                description = "API документация для онлайн-библиотеки"
+        ),
+        security = @SecurityRequirement(name = "Bearer Authentication")
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+
+}
